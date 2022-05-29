a3d82fa26431aa65c51f4d3f068556e3ce9f9d3b
buzz1980 <105562851+buzz1980@users.noreply.github.com>
2022-05-29 11:14:20 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,9 +1,69 @@
 input.onButtonPressed(Button.A, function () {
+    soundExpression.twinkle.play()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showLeds(`
-        . # # # .
-        # . . . .
-        # . . . .
-        # . . . #
-        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
         `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Duck)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    soundExpression.spring.play()
 })
